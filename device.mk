32787ffecc4782bf0dc2991edd2b34c9ac7e1e7b
MomoYuuki <2711300270@qq.com>
2025-10-09 23:13:58 +0800

    fussi: support Oneplus Camera

--- device.mk
+++ device.mk
@@ -27,6 +27,9 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
+# Oplus Camera
+$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
+
 # Commondcs
 PRODUCT_PACKAGES += \
     vendor.oplus.hardware.commondcs-service
@@ -34,7 +37,7 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display_id_4630946358012694402.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946358012694402.xml
-    
+
 # Lineage Health
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/oplus_chg/battery/mmi_charging_enable)
 
