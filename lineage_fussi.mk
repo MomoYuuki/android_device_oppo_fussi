c7dad66afc2f13b02196cb8929ec7874b06b8cef
MomoYuuki <2711300270@qq.com>
2025-10-18 23:31:17 +0800

    fussi: Hello RisingOS

--- lineage_fussi.mk
+++ lineage_fussi.mk
@@ -21,6 +21,18 @@ PRODUCT_MANUFACTURER := OPPO
 PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := CPH2173
 
+# CORE build flags
+WITH_GMS := true
+
+# Disable/enable blur support, false by default
+TARGET_ENABLE_BLUR := true
+
+# Ship Pixel Launcher
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+
+# Whether to ship prebuilt Google Dialer and Messages, false by default
+TARGET_INCLUDE_GOOGLE_DIALER := true
+
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -29,4 +41,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=OPPOFindX3Pro \
     DeviceProduct=OPPOFindX3Pro \
     SystemDevice=OPPOFindX3Pro \
-    SystemName=OPPOFindX3Pro
+    SystemName=OPPOFindX3Pro    \
+    RisingChipset="Snapdragon 888" \
+    RisingMaintainer="MomoYuuki"
